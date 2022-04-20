ce2171c72a0440b613f12e0b1dddf1109b12acfd
Justin Herndon <jherndon111@gmail.com>
2022-04-19 22:06:12 -0500

    first draft of PA3

---
+++ pa3/test.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<random>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[]){
+
+int devices = std::atoi(argv[1]); 
+int counter = 1;
+int breakCondition = 0;	
+int device[devices];
+
+// Linear Backoff
+while(1){
+	int windowSize[counter + 1]; 
+	int check = counter + 1;
+
+	for(int a = 0; a < check; a++){
+		windowSize[a] = a;
+	}
+
+	for(int i =0; i <devices;i++){
+		// Generating a random number from stackoverflow.com/questions/7560114/random-number-c-in-some-range
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> distr(0,counter);
+
+		device[i] = distr(gen);
+
+		for(int b = 0; b < check; b++){
+			if(device[i] == windowSize[b]){
+        			device[i] = -1;
+			}
+		}
+	}
+
+	for(int c= 0; c < devices; c++){
+		if(device[c]==-1){
+			breakCondition += 1;		
+		}
+	}
+	
+	if(breakCondition == devices){
+		break;
+	}
+	
+	breakCondition = 0;		
+	counter += 1;
+}
+
+
+// Binary Exponential Backoff
+
+
+
+// LogLog Backoff
+
+
+
+return 0;
+}
